87e9d1bdd5883ff14a31dc3935ea8ff7a571dca2
anilbanarsee <anil.banarsee@googlemail.com>
2016-10-16 16:42:40 +0100

    Moved Clip and UserLog to discordbot.wilsonbot

--- src/main/java/abanstudio/discordbot/wilson/Clip.java
+++ src/main/java/abanstudio/discordbot/wilson/Clip.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package abanstudio.discordbot;
+package abanstudio.discordbot.wilson;
 
 import java.io.File;
 
--- src/main/java/abanstudio/discordbot/wilson/UserLog.java
+++ src/main/java/abanstudio/discordbot/wilson/UserLog.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package abanstudio.discordbot;
+package abanstudio.discordbot.wilson;
 
 import abanstudio.exceptions.R9KException;
 import java.time.LocalTime;
