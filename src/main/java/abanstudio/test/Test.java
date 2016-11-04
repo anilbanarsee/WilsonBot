55393e256ef009018d0307a016838c8e84325ffe
anilbanarsee <anil.banarsee@googlemail.com>
2016-11-04 10:27:01 +0000

    Removed test java files

--- src/main/java/abanstudio/test/Test.java
+++ src/main/java/abanstudio/test/Test.java
@@ -8,10 +8,18 @@ package abanstudio.test;
 import abanstudio.command.Action;
 import abanstudio.command.Command;
 import abanstudio.utils.sqlite.DBHandler;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import org.apache.commons.codec.binary.Base64;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.audio.AudioPlayer;
 import sx.blah.discord.util.audio.providers.FileProvider;
@@ -21,13 +29,24 @@ import sx.blah.discord.util.audio.providers.FileProvider;
  * @author User
  */
 public class Test {
-    public static void main(String[] args){
-        ArrayList<String> test = new ArrayList<>();
-        test.add("hello");
-        test.add("world");
-        String[] array = new String[test.size()];
-        String[] testarray = test.toArray(array);
-        System.out.println(Arrays.toString(testarray));
+    public static void main(String[] args) throws MalformedURLException, UnsupportedEncodingException, IOException{
+        /*curl -i -H 'Accept: application/vnd.twitchtv.v2+json'\
+-H 'Client-ID: axjhfp777tflhy0yjb5sftsil'\
+'https://api.twitch.tv/kraken/channels/hebo'*/
+        
+        
+   String stringUrl = "https://api.twitch.tv/kraken/channels/hebo";
+        URL url = new URL(stringUrl);
+        URLConnection uc = url.openConnection();
+
+        uc.setRequestProperty("X-Requested-With", "Curl");
+
+        String userpass = "username" + ":" + "password";
+        String basicAuth = "Basic " + new String(new Base64().encode(userpass.getBytes()));
+        uc.setRequestProperty("Authorization", basicAuth);
+
+        InputStreamReader inputStreamReader = new InputStreamReader(uc.getInputStream());
+   
     }
     public static void testMethod(String s){
         System.out.println(s.length());
